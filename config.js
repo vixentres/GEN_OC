47c98f843fa347f26f3df5d83c8ac81c52ee1fcb
vixentres <vicente534mc@gmail.com>
2026-04-21 15:00:05 -0400

    Update config.js

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
 // Este archivo actúa como tu .env para GitHub Pages.
 // Git ignora los .env nativos en frontend, así que se usa este script de configuración.
 const env = {
-    SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzZNXvogW0YWxt6F70MqnMEK0MsmE8vbHtepL3Fs7bLgXIt9DP7s1u2OpD-hyG8fVc/exec"
+    SCRIPT_URL: "https://script.google.com/macros/s/AKfycbytFN5fQDVzDXsDhIWsrOAQdOHeRFiItNJyaK_cdQDGbd7kFMO9zt-6CjuhUJrqO9h8/exec"
 };

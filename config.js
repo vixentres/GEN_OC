2b167078cb7dc652e51ec5d7b18b65b11f5359e4
vixentres <vicente534mc@gmail.com>
2026-04-21 15:03:10 -0400

    Add files via upload

--- config.js
+++ config.js
@@ -1,6 +1,4 @@
 // config.js
-// Este archivo actúa como tu .env para GitHub Pages.
-// Git ignora los .env nativos en frontend, así que se usa este script de configuración.
 const env = {
     SCRIPT_URL: "https://script.google.com/macros/s/AKfycbytFN5fQDVzDXsDhIWsrOAQdOHeRFiItNJyaK_cdQDGbd7kFMO9zt-6CjuhUJrqO9h8/exec"
 };

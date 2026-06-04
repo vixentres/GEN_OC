954b9fea0c528bd1ba6c584ecb82877454941ec5
vixentres <vicente534mc@gmail.com>
2026-06-04 11:05:00 -0400

    chore: update script URL in config and index fallback

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // config.js
 const env = {
-    SCRIPT_URL: "https://script.google.com/macros/s/AKfycbyEnhfw2WswZWgKiKrqWMlHCSQoVKzFiL9WGJEVI7ZcW1gzfC3KFrTQpF17rhnqRIo/exec"
+    SCRIPT_URL: "https://script.google.com/macros/s/AKfycbxiPttFUAzBGmMLL2VcQZ_qhjXYVh7owa-vkJbKNpRKCV9sfKzxfB6A9csmlwq6Tpq3/exec"
 };
